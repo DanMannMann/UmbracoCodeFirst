a1fb555f05b859b456476044840d80234ab30280
DanMannMann <dan@marsman.co.uk>
2016-02-13 18:26:48 +0000

    initial commit after conversion

--- reset.bat
+++
@@ -1,6 +0,0 @@
-E:
-cd E:\tfs\DefaultCollection\Felinesoft.UmbracoCodeFirst\Dev Branches\twigs\refactor-converters
-copy "DB restore\Umbraco - uninitialised.sdf" "Demo\App_Data"
-del /q "Demo\App_Data\Umbraco.sdf"
-ren "Demo\App_Data\Umbraco - uninitialised.sdf" "Umbraco.sdf"
-pause
