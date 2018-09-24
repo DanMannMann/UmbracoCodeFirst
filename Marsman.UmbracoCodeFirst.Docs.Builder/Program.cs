1d1c6d9bb5a0b65970f62b92dd4448284f96f0b9
DanMannMann <dan@marsman.co.uk>
2018-09-24 15:22:00 +0100

    build help using nuget package

---
+++ Marsman.UmbracoCodeFirst.Docs.Builder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marsman.UmbracoCodeFirst.Docs.Builder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// This project is used to install the SHFB nuget package and build the Docs project on the build server.
+		}
+	}
+}
